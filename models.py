ab0518457d242727a531c9fe207731216606942e
Praveen Kumar S <146429298+Praveenkumar1104@users.noreply.github.com>
2025-03-20 00:18:21 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class CyberbullyingText(models.Model):
-    text = models.TextField()
-    is_cyberbullying = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.text[:50]
